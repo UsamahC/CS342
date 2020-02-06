08d0b16bbeb6025da6deb5282214920eb9035481
UsamahC <32469359+UsamahC@users.noreply.github.com>
2020-02-06 01:54:03 -0500

    Update netid_grouping.py

--- netid_grouping.py
+++ netid_grouping.py
@@ -1,9 +1,8 @@
 import numpy
 
-
 #Naming of text files - enter file names here
 a = "netids.txt"
-b = "prefs.txt"
+b = "netidsa.txt"
 
 #read in the text file with netids
 f = open(a, "r")
@@ -22,6 +21,7 @@ num_groups = len(lines2)
 double_groups = []
 single_groups = []
 
+num0 = []
 num1 = []
 num2 = []
 num3 = []
@@ -29,18 +29,21 @@ num4 = []
 num5 = []
 num6 = []
 num7 = []
+num8 = []
 
 
 
 
 #iterate through all of the preferences
 for i in lines2:
-    values = [len(num1), len(num2), len(num3), len(num4), len(num5), len(num6), len(num7)]
-    group = i.replace(",", "")
-    group = group.split(" ")
-
-    if len(group) == 5:
-        if values[int(group[2])-1] <= values[int(group[3])-1] and values[int(group[2])-1] <= values[int(group[4])-1]:
+    values = [len(num0), len(num1), len(num2), len(num3), len(num4), len(num5), len(num6), len(num7), len(num8)]
+    #group = i.replace(",", "")
+    group = i.split()
+    #print group
+    if 1 == 1:
+        if values[int(group[2])] <= values[int(group[3])] and values[int(group[2])] <= values[int(group[4])]:
+            if int(group[2]) == 0:
+                pass
             if int(group[2]) == 1:
                 num1.append(group[0])
                 num1.append(group[1])
@@ -62,7 +65,12 @@ for i in lines2:
             if int(group[2]) == 7:
                 num7.append(group[0])
                 num7.append(group[1])
-        if values[int(group[3])-1] <= values[int(group[2])-1] and values[int(group[3])-1] <= values[int(group[4])-1]:
+            if int(group[2]) == 8:
+                num8.append(group[0])
+                num8.append(group[1])
+        if values[int(group[3])] <= values[int(group[2])] and values[int(group[3])] <= values[int(group[4])]:
+            if int(group[3]) == 0:
+                pass
             if int(group[3]) == 1:
                 num1.append(group[0])
                 num1.append(group[1])
@@ -84,8 +92,13 @@ for i in lines2:
             if int(group[3]) == 7:
                 num7.append(group[0])
                 num7.append(group[1])
+            if int(group[3]) == 8:
+                num8.append(group[0])
+                num8.append(group[1])
 
-        if values[int(group[4])-1] <= values[int(group[2])-1] and values[int(group[4])-1] <= values[int(group[3])-1]:
+        if values[int(group[4])] <= values[int(group[2])] and values[int(group[4])] <= values[int(group[3])]:
+            if int(group[4]) == 0:
+                pass
             if int(group[4]) == 1:
                 num1.append(group[0])
                 num1.append(group[1])
@@ -107,79 +120,32 @@ for i in lines2:
             if int(group[4]) == 7:
                 num7.append(group[0])
                 num7.append(group[1])
+            if int(group[4]) == 8:
+                num8.append(group[0])
+                num8.append(group[1])
 
-    if len(group) == 4:
-        if values[int(group[1])-1] <= values[int(group[2])-1] and values[int(group[1])-1] <= values[int(group[3])-1]:
-            if int(group[1]) == 1:
-                num1.append(group[0])
-            if int(group[1]) == 2:
-                num2.append(group[0])
-            if int(group[1]) == 3:
-                num3.append(group[0])
-            if int(group[1]) == 4:
-                num4.append(group[0])
-            if int(group[1]) == 5:
-                num5.append(group[0])
-            if int(group[1]) == 6:
-                num6.append(group[0])
-            if int(group[1]) == 7:
-                num7.append(group[0])
-        if values[int(group[2])-1] <= values[int(group[1])-1] and values[int(group[2])-1] <= values[int(group[3])-1]:
-            if int(group[2]) == 1:
-                num1.append(group[0])
-            if int(group[2]) == 2:
-                num2.append(group[0])
-            if int(group[2]) == 3:
-                num3.append(group[0])
-            if int(group[2]) == 4:
-                num4.append(group[0])
-            if int(group[2]) == 5:
-                num5.append(group[0])
-            if int(group[2]) == 6:
-                num6.append(group[0])
-            if int(group[2]) == 7:
-                num7.append(group[0])
-
-        if values[int(group[3])-1] <= values[int(group[1])-1] and values[int(group[3])-1] <= values[int(group[2])-1]:
-            if int(group[3]) == 1:
-                num1.append(group[0])
-            if int(group[3]) == 2:
-                num2.append(group[0])
-            if int(group[3]) == 3:
-                num3.append(group[0])
-            if int(group[3]) == 4:
-                num4.append(group[0])
-            if int(group[3]) == 5:
-                num5.append(group[0])
-            if int(group[3]) == 6:
-                num6.append(group[0])
-            if int(group[3]) == 7:
-                num7.append(group[0])
 
 #splitting preferences into groups of 4:
-def splitter(l, n):
+def splitter(l, n, a):
     list = [l[i * n:(i + 1) * n] for i in range((len(l) + n - 1) // n )]
-    print list
+    for i in list:
+        print ' '.join(str(x) for x in i), a
 
 
-print "Preference 1:"
-splitter(num1, 4)
+
+splitter(num1, 4, "1")
 print "\n"
-print "Preference 2:"
-splitter(num2, 4)
+splitter(num2, 4, "2")
 print "\n"
-print "Preference 3:"
-splitter(num3, 4)
+splitter(num3, 4, "3")
 print "\n"
-print "Preference 4:"
-splitter(num4, 4)
+splitter(num4, 4, "4")
 print "\n"
-print 'Preference 5:'
-splitter(num5, 4)
+splitter(num5, 4, "5")
 print "\n"
-print 'Preference 6:'
-splitter(num6, 4)
+splitter(num6, 4, "6")
 print "\n"
-print "Preference 7;"
-splitter(num7, 4)
+splitter(num7, 4, "7")
+print "\n"
+splitter(num7, 4, "8")
 print "\n"
